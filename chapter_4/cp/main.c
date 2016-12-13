4d12138d97d56944e68274a616b2255dcb9025ca
Caspar Rinne <caspar.rinne@gmail.com>
2016-12-13 22:20:50 +0200

    Adding test file with file hole

--- chapter_4/cp/main.c
+++ chapter_4/cp/main.c
@@ -2,35 +2,60 @@
 #include <sys/stat.h>
 #include <fcntl.h> // open
 #include <unistd.h>
-
+#include <string.h>
 
 #define BUF_SIZE 1024
 
 int main(int argc, char *argv[])
 {
 
-    if (argc < 2)
+    if (argc < 2) {
         exit(EXIT_SUCCESS);
-    
+    }
+
     // open file
-    int inputFd = open(argv[1],O_RDWR);
-
-    if (inputFd == -1)
+    int inputFd;
+    if ((inputFd = open(argv[1],O_RDWR)) == -1) {
         exit(EXIT_FAILURE);
+    }
 
-    lseek(inputFd, 10, SEEK_SET);
+    /* creating test file
+    char buffer[] = "This is my test file \n and second rot\n";
+    int outputFd;
+    if ((outputFd = open("test2.txt", O_RDWR)) != -1) {
+        if (lseek(outputFd, 100, SEEK_SET) == -1) {
+            exit(EXIT_FAILURE);
+        }
+
+        if (write(outputFd, buffer, strlen(buffer)) == -1) {
+            printf("Err\n");
+        }
+    }
+*/
+    //lseek(inputFd, 10, SEEK_SET);
+    
     ssize_t numRead;
     char buf[BUF_SIZE + 1];
     
     while ((numRead = read(inputFd, buf, BUF_SIZE)) > 0) {
         //buf[numRead] = '\0';
-        if (write(STDOUT_FILENO, buf, numRead) != numRead) {
-            exit(EXIT_FAILURE);
+        
+        for (int i = 0; i < numRead; i++) {    
+            if (buf[i] == '\0') {
+                printf("Hole\n");
+            } 
+            else {
+                printf("%c\n", buf[i]);
+            }
+            //if (write(STDOUT_FILENO, buf, numRead) != numRead) {
+            //    exit(EXIT_FAILURE);
+           // }
         }
     }
 
-    if (close(inputFd) == -1)
+    if (close(inputFd) == -1) {
         exit(EXIT_FAILURE); 
-
+    }
+    
     exit(EXIT_SUCCESS);
 }
