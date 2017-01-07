41a9ae211acfc9989b9de8f658308c6f93a02888
Caspar Rinne <caspar.rinne@gmail.com>
2017-01-07 19:23:36 +0200

    lpi-ex5-2 Remove exercise description

--- chapter_5/ex5_2/main.c
+++ chapter_5/ex5_2/main.c
@@ -1,12 +1,6 @@
 #include "headers.h"
 #define BUF_LEN 1024
 
-/*
- Write a program that opens an existing file for writing with the O_APPEND flag, and
- then seeks to the beginning of the file before writing some data. Where does the
- data appear in the file? Why?
- */
-
 int main(int argc,char *argv[]) 
 {
     if (argc !=2 || strcmp(argv[1], "--help") == 0) { 
