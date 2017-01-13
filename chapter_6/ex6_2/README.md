50ddaa180580f8e70d93069c0768d7b48e306464
Caspar Rinne <caspar.rinne@gmail.com>
2017-01-13 06:57:47 +0200

    lpi-ex6-2 Add solution for exercise 6-2

---
+++ chapter_6/ex6_2/README.md
@@ -0,0 +1,3 @@
+**Exercise**
+
+Write a program to see what happens if we try to longjmp() into a function that has already returned.
---
+++ chapter_6/ex6_2/main.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <setjmp.h> /* setjump and longjmp */
+
+/* array type suitable for holfing the information
+    needed to restore a calling environment */
+static jmp_buf env;
+
+static void
+foo(int argc)
+{
+    if (argc) {
+        /* setjmp saves the state */
+        switch (setjmp(env)) {
+            case 0: /* This is the return after the initial setjmp() */
+                printf("Case 0\n");
+                break;
+            case 1:
+                printf("Case 1\n");
+                break;
+            case 2:
+                printf("Case 2\n");
+                break;
+            }
+    }
+}
+
+int
+main(int argc, char *argv[])
+{
+    foo(0);
+    //foo(1);
+    longjmp(env, 0);
+    exit(EXIT_SUCCESS);
+}
